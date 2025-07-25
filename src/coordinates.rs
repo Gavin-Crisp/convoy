216c9ccef6415205fc6a3f253b16de1cec23981c
Gavin-Crisp <gavin@gavincrisp.com.au>
2025-07-25 10:39:55 +0930

    Added + and - for Coord

--- src/coordinates.rs
+++ src/coordinates.rs
@@ -1,4 +1,5 @@
 use std::fmt::{Display, Formatter};
+use std::ops::{Add, AddAssign, Sub, SubAssign};
 
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq)]
 pub struct Coord {
@@ -33,5 +34,41 @@ impl Coord {
 impl Display for Coord {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}:{})", self.rank, self.file)
+    }
+}
+
+impl Add for Coord {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            rank: self.rank.wrapping_add(rhs.rank),
+            file: self.file.wrapping_add(rhs.file),
+        }
+    }
+}
+
+impl Sub for Coord {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            rank: self.rank.wrapping_sub(rhs.rank),
+            file: self.file.wrapping_sub(rhs.file),
+        }
+    }
+}
+
+impl AddAssign for Coord {
+    fn add_assign(&mut self, rhs: Self) {
+        self.rank += rhs.rank;
+        self.file += rhs.file;
+    }
+}
+
+impl SubAssign for Coord {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.rank -= rhs.rank;
+        self.file -= rhs.file;
     }
 }
