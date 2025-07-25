9ed7c07e8bec12193642fbe6dfcd06e370b2eacd
Gavin-Crisp <gavin@gavincrisp.com.au>
2025-07-25 10:39:40 +0930

    Added pieces mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
 pub mod coordinates;
+pub mod piece;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Player {
+    P1,
+    P2
+}
---
+++ src/piece.rs
@@ -0,0 +1,69 @@
+use crate::Player;
+use std::ops::Range;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct Piece {
+    kind: PieceType,
+    exhausted: bool,
+    owner: Player,
+}
+
+impl Piece {
+    #[must_use]
+    pub const fn kind(self) -> PieceType {
+        self.kind
+    }
+
+    #[must_use]
+    pub const fn exhausted(self) -> bool {
+        self.exhausted
+    }
+
+    #[must_use]
+    pub const fn owner(self) -> Player {
+        self.owner
+    }
+
+    #[must_use]
+    pub const fn speed(self) -> u8 {
+        match self.kind {
+            PieceType::Artillery | PieceType::Infantry => 2,
+            PieceType::Convoy | PieceType::Recon => 3,
+        }
+    }
+
+    #[must_use]
+    pub const fn power(self) -> u8 {
+        match self.kind {
+            PieceType::Artillery | PieceType::Infantry => 2,
+            PieceType::Convoy => 0,
+            PieceType::Recon => 1,
+        }
+    }
+
+    #[must_use]
+    pub const fn range(self) -> Range<u8> {
+        match self.kind {
+            PieceType::Artillery => 2..4,
+            PieceType::Convoy => 0..0,
+            PieceType::Infantry | PieceType::Recon => 1..2,
+        }
+    }
+
+    #[must_use]
+    pub const fn cost(self) -> u8 {
+        match self.kind {
+            PieceType::Artillery | PieceType::Convoy => 3,
+            PieceType::Infantry => 2,
+            PieceType::Recon => 4,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum PieceType {
+    Artillery,
+    Convoy,
+    Infantry,
+    Recon,
+}
